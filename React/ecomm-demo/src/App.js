b05099e2773faf8a8732907018aa9caf3982f9e3
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-13 21:47:39 +0530

    EComm Demo - Initial Code

---
+++ React/ecomm-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Mobile Shop</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
