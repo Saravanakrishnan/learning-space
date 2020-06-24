04aac6f8845e5d7414ad9493344d0ed9063ee6ba
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-06-24 20:42:39 +0530

    full-stack app - initial commit

---
+++ full-stack/credit-card-demo-app/fullstack/src/main/java/com/github/fullstack/CreditCardFullStackAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardFullStackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardFullStackAppApplication.class, args);
+	}
+
+}
---
+++ full-stack/credit-card-demo-app/fullstack/src/test/java/com/github/fullstack/CreditCardFullStackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardFullStackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
